d09f3ea15fce8bb48bedb82707abb8278e276f19
V. Raina <vr313@air208>
2019-10-12 12:53:01 +0100

    read in word list

--- adversarial_attack/greedy_word_search.py
+++ adversarial_attack/greedy_word_search.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 import torch
-from model_structures import ThreeLayer_1LevelAttn_RNN
+from models import ThreeLayerNet_1LevelAttn_RNN
 
 
 # Get the part and its seed to adversarially attack
@@ -20,4 +20,23 @@ model_path = '../saved_bert_models/'+trained_model_to_load
 model = torch.load(model_path)
 model.eval()
 
+
+# Load the relevant part 'useful' data
+data_file = '../data/BLXXXeval3/useful_part'+str(part)+'.txt'
+with open(data_file, 'r') as f:
+	utterances = json.loads(f.read())
+
+# Convert json output from unicode to string
+utterances = [[str(item[0]), str(item[1])] for item in utterances]
+
+
+# Create list of words to iterate through for appending
+words_file = '/home/alta/BLTSpeaking/lms/LM15-grph/wlists/train.lst'
+with open(words_file, 'r') as f:
+	test_words = f.readlines()
+test_words = [str(word).lower() for word in test_words]
+print(len(test_words))
+
+for i in range(20):
+	print(test_words[i])
  
